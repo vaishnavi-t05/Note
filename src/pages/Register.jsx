bcb1df9eab8e58a4d17f8a9fd2662e41982108f7
vaishnavi-t05 <tvaishnavi2205@gmail.com>
2026-08-18 11:53:43 +0530

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./register.css";
+import "./Register.css";
 
 function Register() {
   const navigate = useNavigate();
